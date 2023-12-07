06090caed36085cd6ad40440ba83fc0e8811a26d
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-12-07 22:20:59 +0800

    Restore hooks

--- src/config/ky.config.ts
+++ src/config/ky.config.ts
@@ -5,20 +5,20 @@ const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY;
 
 export const kyInstance = ky.extend({
   prefixUrl,
-  // hooks: {
-  //   beforeRequest: [
-  //     (options) => {
-  //       // Get token from localstorage
-  //       const token = JSON.parse(localStorage.getItem(TOKEN_KEY) || '{}') || {};
-  //       const { access_token: accessToken } = token;
-  //       // If token is present then add authorization to header
-  //       if (accessToken) {
-  //         options.headers.set('Authorization', `Bearer ${accessToken}`);
-  //       }
-  //     },
-  //   ],
-  //   afterResponse: [],
-  // },
+  hooks: {
+    beforeRequest: [
+      (options) => {
+        // Get token from localstorage
+        const token = JSON.parse(localStorage.getItem(TOKEN_KEY) || '{}') || {};
+        const { access_token: accessToken } = token;
+        // If token is present then add authorization to header
+        if (accessToken) {
+          options.headers.set('Authorization', `Bearer ${accessToken}`);
+        }
+      },
+    ],
+    afterResponse: [],
+  },
 });
 
 export function generateSearchParams(query: {
