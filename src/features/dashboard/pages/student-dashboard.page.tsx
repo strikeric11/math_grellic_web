ffc4e54585dc574143b2c6b7e0543060d15572c2
Erwin Errol Calanuga <hortsatta@gmail.com>
2024-02-07 17:57:42 +0800

    Fix dashboard components max width

--- src/features/dashboard/pages/student-dashboard.page.tsx
+++ src/features/dashboard/pages/student-dashboard.page.tsx
@@ -43,7 +43,7 @@ export function StudentDashboardPage() {
 
   return (
     <div className='max-w-auto mx-auto flex w-full flex-col items-center justify-center gap-5 pb-8 sm:max-w-[592px] -2lg:max-w-[835px] xl:flex-row xl:items-start'>
-      <div className='xl:max-w-auto flex w-full shrink-0 flex-col gap-5 xl:w-[592px] xl:pb-8 2xl:w-auto 2xl:min-w-[835px]'>
+      <div className='xl:max-w-auto flex w-full shrink-0 flex-col gap-5 xl:w-[592px] xl:pb-8 2xl:w-auto 2xl:max-w-[835px]'>
         <StudentDashboardUserSummary
           className='min-h-[262px]'
           user={user}
@@ -71,7 +71,7 @@ export function StudentDashboardPage() {
       </div>
       <div className='flex w-full flex-col gap-5 -2lg:w-fit'>
         <StudentDashboardAnnouncementList
-          className='hidden -2lg:block'
+          className='hidden min-h-[262px] -2lg:block'
           loading={announcementListLoading}
           studentAnnouncements={studentAnnouncements}
           onRefresh={refreshAnnouncements}
--- src/features/dashboard/pages/teacher-dashboard.page.tsx
+++ src/features/dashboard/pages/teacher-dashboard.page.tsx
@@ -57,7 +57,7 @@ export function TeacherDashboardPage() {
 
   return (
     <div className='max-w-auto mx-auto flex w-full flex-col items-center justify-center gap-5 pb-8 sm:max-w-[592px] -2lg:max-w-[835px] xl:flex-row xl:items-start'>
-      <div className='xl:max-w-auto flex w-full shrink-0 flex-col gap-5 xl:w-[592px] xl:pb-8 2xl:w-auto 2xl:min-w-[835px]'>
+      <div className='xl:max-w-auto flex w-full shrink-0 flex-col gap-5 xl:w-[592px] xl:pb-8 2xl:w-auto 2xl:max-w-[835px]'>
         <TeacherDashboardUserSummary
           className='min-h-[262px]'
           user={user}
