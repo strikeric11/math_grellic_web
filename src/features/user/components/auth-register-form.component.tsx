81ff8790a1a211898cbb0d81a94c4b773d6154ca
strikeric11 <strikeric11@gmail.com>
2024-09-22 15:03:37 +1000

    Update auth-register-form.component.tsx

--- src/features/user/components/auth-register-form.component.tsx
+++ src/features/user/components/auth-register-form.component.tsx
@@ -136,8 +136,8 @@ export const AuthRegisterForm = memo(function ({
 
   const submitForm = useCallback(
     async (data: AuthRegisterFormData) => {
-      console.log('Server Response data a:', data);
-      console.log('Server Response userRole a:', userRole);
+      console.log('Server Response data b:', data);
+      console.log('Server Response userRole b:', userRole);
       try {
         const response = await onSubmit(data, userRole);
         console.log('Server Response 1:', response);
